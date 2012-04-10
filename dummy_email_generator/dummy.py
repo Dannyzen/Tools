497d944ca86f857448f1a898d4ea73e32ad21003
Danny Rosen <danny.rosen@gmail.com>
2012-04-10 19:45:25 -0400

    looks good from here

--- dummy_email_generator/dummy.py
+++ dummy_email_generator/dummy.py
@@ -1,6 +1,6 @@
 #simple random email generator using my sailthru address (danny rosen 4/10/2012)
 #usage: dummy.py -n # - outputs to a fakes.txt file.
-
+#twitter: @dannyzen
 import argparse
 parser = argparse.ArgumentParser(description='Docs docs docs these are my docs')
 parser.add_argument('-n','--n_variable', type=int, help='Number of fake emails to create', required=True)
@@ -9,7 +9,6 @@ args = vars(parser.parse_args())
 f = open('fakes.txt','w')
 
 x = args['n_variable']
-print x
 while x != 0:
     x = x - 1
     f.write ("danny+test" + str(x) + "@sailthru.com\n")
