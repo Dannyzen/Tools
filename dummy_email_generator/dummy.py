ffd8e8bdd4ceb5c24687c7902df4e3882a22fb59
Danny Rosen <danny.rosen@gmail.com>
2012-04-10 19:28:44 -0400

    oneliner docs ftw

--- dummy_email_generator/dummy.py
+++ dummy_email_generator/dummy.py
@@ -1,10 +1,12 @@
 #simple random email generator using my sailthru address (danny rosen 4/10/2012)
+#usage: dummy.py -n # - outputs to a fakes.txt file.
+
 import argparse
 parser = argparse.ArgumentParser(description='Docs docs docs these are my docs')
 parser.add_argument('-n','--n_variable', type=int, help='Number of fake emails to create', required=True)
 args = vars(parser.parse_args())
 
-f = open('foo.txt','w')
+f = open('fakes.txt','w')
 
 x = args['n_variable']
 print x
