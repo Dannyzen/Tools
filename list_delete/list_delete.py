343ff98e429bc291a800ff7bb74cce8532b51591
Ubuntu <ubuntu@ip-10-196-81-248.ec2.internal>
2012-04-15 04:44:03 +0000

    got the listnames now

--- list_delete/list_delete.py
+++ list_delete/list_delete.py
@@ -15,7 +15,7 @@ args = parser.parse_args()
 env = args.e_variable
 
 #open the data file with the api info
-with open('/Users/robertrosen/d.txt') as json_data:
+with open('/home/ubuntu/d.txt') as json_data:
     data = json.load(json_data)
     api_key = data["api_key"]
     api_secret = data["api_secret"]
@@ -27,14 +27,5 @@ except KeyError:
 sailthru_client = sc.SailthruClient(api_key, api_secret, env_url)
 response = sailthru_client.api_get('list', data)
 jdata = json.loads(response)
-pprint(jdata["lists"])
-"""
-outpout looks like this:
-[{u'create_time': u'Thu, 12 Apr 2012 16:40:19 -0400',
-  u'email_count': 1,
-  u'name': u'list1'},
- {u'create_time': u'Thu, 12 Apr 2012 16:38:47 -0400',
-  u'email_count': 0,
-  u'name': u'list2'},}]
-I can't figure out how to store the values of jdata.lists.name in a list that would look like this: ["list1","list2","list3"...]
-"""
+print([l['name'] for l in jdata['lists']]) 
+
