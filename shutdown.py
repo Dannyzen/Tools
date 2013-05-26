7a21437ef08c26b99581299cc9c9ec42a9326a06
Danny Rosen <danny.rosen@gmail.com>
2013-05-25 23:20:38 -0400

    minor cleanup and simplification. not everything in life needs to be fancy.

--- shutdown.py
+++ shutdown.py
@@ -1,22 +1,15 @@
 import sys
 import os
 
-class Shutdown:
-	def __init__(self):
-		if len(sys.argv) == 1:
-			shutdown_stop()
-		else:
-			shutdown_time(sys.argv[1])
-
 def shutdown_stop():
 	os.system("shutdown /a")
 
 def shutdown_time(time):
 	time = int(time) * 60
-	print(type(time))
 	shutdown_string = "shutdown /s /t " + str(time)
-	print shutdown_string
 	os.system(shutdown_string)
 
-if __name__ == "__main__":
-	Shutdown()
+if len(sys.argv) == 1:
+	shutdown_stop()
+else:
+	shutdown_time(sys.argv[1])
