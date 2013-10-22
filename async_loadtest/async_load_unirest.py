b54b8e3aa93763956f99877c2aeb75f5a181d238
Danny Rosen <danny.rosen@gmail.com>
2013-10-22 09:55:16 -0400

    cleaner

--- async_loadtest/async_load_unirest.py
+++ async_loadtest/async_load_unirest.py
@@ -4,17 +4,13 @@ destination =  "http://httpbin.org/post"
 num = 500
 
 def callback(response):
-  response.code # The HTTP status code
   print response.code
-  response.headers # The HTTP headers
   print response.headers
-  response.body # The parsed response
   print response.body
-  response.raw_body # The unparsed response
   print response.raw_body
 
 def run(num, destination):
     for n in range(0, num):
         thread = unirest.post(destination, { "Accept": "application/json" }, { "parameter": 23, "foo": "bar" }, callback)
-        print thread
+
 run(num, destination)
