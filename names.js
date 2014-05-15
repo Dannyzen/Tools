46ef1bde69871ac7adb3c8f0686ccf312c24daf5
Danny Rosen <danny.rosen@gmail.com>
2014-05-15 18:39:19 -0400

    yay names

---
+++ names.js
@@ -0,0 +1,16 @@
+var github = require('octonode');
+var client = github.client('');
+var inspect = require('util').inspect;
+
+console.log('TEST');
+
+client.get('/orgs/sailthru/members', {per_page:100}, function (err, status, body, headers) {
+        console.log('Sort!');
+        
+        body.sort(function (a,b) { return a.login.toLowerCase() > b.login.toLowerCase() ?1 :-1 ; });
+        
+        for(var i = 0; i < body.length; i++){
+            console.log(body[i].login);
+        }
+    }
+);
